610e27614a8665844783c54e31de7a565a3c37e2
prasadrotty <144917901+prasadrotty@users.noreply.github.com>
2026-06-05 12:12:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,17 @@ st.set_page_config(
   layout="wide",                 # "centered" or "wide"
   initial_sidebar_state="expanded"
 )
+file_path="healthy_diet_calorie_intake.csv"
 
-try:
-   df=pd.read_csv("healthy_diet_calorie_intake.csv")
-   st.sub_headder("Dataset preview")
-   st.dataframe(df)
-   st.write("Displaying the Dataset",df.shape)
-   st.write("Displaying the Features :", df.columns)
- except Exception as e:
-   st.error(f"file not found : {e}")
+if os.path.exists(file_path):
+  try:
+    df=pd.read_csv("healthy_diet_calorie_intake.csv")
+    st.sub_headder("Dataset preview")
+    st.dataframe(df)
+    st.write("Displaying the Dataset",df.shape)
+    st.write("Displaying the Features :", df.columns)
+   except Exception as e:
+    st.error(f"file not found : {e}")
 
 else:
   st.info("Plese upload the file")
