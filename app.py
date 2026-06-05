35492baab7a17e2e26a78e5893ce08920ea3c512
prasadrotty <144917901+prasadrotty@users.noreply.github.com>
2026-06-05 11:09:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+# Set pahe config
+st.set_page_config(
+  page_title="Shree's Awesome APP",
+  page_icon="🚀",
+  layout="wide",                 # "centered" or "wide"
+  initial_sidebar_state="expanded"
+)
