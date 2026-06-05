3584e6e2855596d004a82843156a75651ffd515f
prasadrotty <144917901+prasadrotty@users.noreply.github.com>
2026-06-05 12:09:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ st.set_page_config(
 )
 
 try:
-  df=pd.read_csv("healthy_diet_calorie_intake.csv")
-  st.sub_headder("Dataset preview")
-  st.dataframe(df)
-  st.write("Displaying the Dataset",df.shape)
-  st.write("Displaying the Features :", df.columns)
-except Exception as e:
-  st.error(f"file not found : {e}")
+   df=pd.read_csv("healthy_diet_calorie_intake.csv")
+   st.sub_headder("Dataset preview")
+   st.dataframe(df)
+   st.write("Displaying the Dataset",df.shape)
+   st.write("Displaying the Features :", df.columns)
+ except Exception as e:
+   st.error(f"file not found : {e}")
 
 else:
   st.info("Plese upload the file")
